c79a68f08025c86dda053b9008e103b5e9cba9d7
ronithussain <xayaanhussain542@gmail.com>
2025-02-23 16:16:02 +0600

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,10 @@
-import AboutPage from '../components/AboutPage';
+
 import Carousel from '../components/Carousel';
-import HomeCard from '../components/HomeCard';
-import MeetOurPartner from '../components/MeetOurPartner';
-import Banner from './shared/Banner';
 
 const Home = () => {
     return (
         <div className='lg:mt-[105px] mt-[105px] '>
             <Carousel></Carousel>
-            <div className='bg-gradient-to-b from-[#1A1618] to-[#1A1618]'>
-            <HomeCard></HomeCard>
-            <AboutPage></AboutPage>
-            </div>
-            <Banner></Banner>
-            <MeetOurPartner></MeetOurPartner>
         </div>
     );
 };
